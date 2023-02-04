510f4875b4da61ac3e40bb71469729addf6f12bd
Ramka <whereisourgod@gmail.com>
2023-02-04 23:07:22 +1000

    task1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 1
+Написать метод/функцию, который/которая на вход принимает массив городов. В качестве результата возвращает строку, где города разделены запятыми, а в конце стоит точка. 
+Пример:
+«Москва, Санкт-Петербург, Воронеж.» */
+
+static string ParseArray(string[] array)
+{
+    string result = "";
+    if (array.Length > 0)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            
+            result+=array[i];
+            if(i == array.Length - 1){
+                result+=".";
+            }else{
+                result+=", ";
+            }
+
+        }
+    }
+    return result;
+}
