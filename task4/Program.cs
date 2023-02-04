f22dcce2e07b824b7fb100c3f98a3efe3e44d554
Ramka <whereisourgod@gmail.com>
2023-02-05 01:04:57 +1000

    task4 done

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 4
+Написать метод/функцию, который/которая на вход принимает целое число, а на выходе возвращает то, является ли число простым (не имеет делителей кроме 1 и самого себя).*/
+
+// Перебор делителей
+
+static bool ifSimpleNumber(int number)
+{
+    if (number <= 1)
+    {
+        return false;
+    }
+    for (int i = 2; i <= Math.Sqrt(number); i++)
+    {
+        if (number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
