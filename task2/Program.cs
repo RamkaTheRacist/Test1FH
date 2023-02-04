61a986a66b1560be5dcc594213f1be4208ad8ab9
Ramka <whereisourgod@gmail.com>
2023-02-04 23:26:27 +1000

    task2 done

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,3 +3,12 @@
 Пример:
 27 => 25, 27.8 => 30, 41.7 => 40.*/
 
+static float RoundNumberToFive(float number)
+{
+    float remainder = number % 5;
+    if (remainder >= 2.5)
+    {
+        return number - remainder + 5;
+    }
+    return number - remainder;
+}
